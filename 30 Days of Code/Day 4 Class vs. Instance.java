3ca8f582fef205513b507accc753f88b97881e3e
Jack Collis <33093310+collis96@users.noreply.github.com>
2019-07-03 12:43:02 +0100

    Create Day 4 Class vs. Instance.java

---
+++ 30 Days of Code/Day 4 Class vs. Instance.java
@@ -0,0 +1,49 @@
+// Author: Jack Collis
+// GitHub: www.github.com/collis96/
+// Portfolio: www.jackcprojects.co.uk
+
+import java.io.*;
+import java.util.*;
+
+public class Person {
+    private int age;	
+  
+	public Person(int initialAge) {
+  		if(initialAge<0){
+            age = 0;
+            System.out.println("Age is not valid, setting age to 0.");
+        } else {
+            age = initialAge;
+        }
+	}
+
+	public void amIOld() {
+  		if(age<13){
+            System.out.println("You are young.");     
+        } else if(age>12 && age<18) {
+            System.out.println("You are a teenager.");
+        } else { //Older than 17
+            System.out.println("You are old.");
+        }
+	}
+
+	public void yearPasses() {
+  		age++;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			int age = sc.nextInt();
+			Person p = new Person(age);
+			p.amIOld();
+			for (int j = 0; j < 3; j++) {
+				p.yearPasses();
+			}
+			p.amIOld();
+			System.out.println();
+        }
+		sc.close();
+    }
+}
