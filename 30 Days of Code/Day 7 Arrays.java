a2e051df1aa5547c726450727273ad27f4fc261b
Jack Collis <33093310+collis96@users.noreply.github.com>
2019-07-03 14:45:27 +0100

    Create Day 7 Arrays.java

---
+++ 30 Days of Code/Day 7 Arrays.java
@@ -0,0 +1,46 @@
+// Author: Jack Collis
+// GitHub: www.github.com/collis96/
+// Portfolio: www.jackcprojects.co.uk
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        scanner.close();
+
+        reverse(arr);
+
+    }
+
+    public static void reverse(int[] arr){
+
+        for(int i=0; i<arr.length ; i++){
+            System.out.print(arr[arr.length - i - 1] + " ");
+        }
+
+    }
+}
